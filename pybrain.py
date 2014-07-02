44264828aeed178fb2c5db1f724bd65ad53abbd3
William Dizon <wdchromium@gmail.com>
2014-07-02 13:07:03 -0700

    trained supervision tuning

--- pybrain.py
+++ pybrain.py
@@ -6,46 +6,43 @@ from pybrain.datasets import SupervisedDataSet
 from time import time
 
 from coup import *
+from itertools import cycle
+from random import random
 
 PLAYERS = 5
+INPUT_NEURONS_PER_PLAYER = 5
+OUTPUT_NEURONS = 5
+HIDDEN_NEURONS = 5
 
-ds = SupervisedDataSet(PLAYERS * 5, 5)
-net = buildNetwork(PLAYERS * 5, 10, 5, bias=True, outputbias= True, hiddenclass=SigmoidLayer)
+ds = SupervisedDataSet(PLAYERS * INPUT_NEURONS_PER_PLAYER, OUTPUT_NEURONS)
+net = buildNetwork(PLAYERS * INPUT_NEURONS_PER_PLAYER, HIDDEN_NEURONS, OUTPUT_NEURONS, bias=True, outputbias= True, hiddenclass=SigmoidLayer)
 trainer = BackpropTrainer(net, ds, learningrate= 0.1)
 
 influences = ['Ambassador', 'Assassin', 'Captain', 'Contessa', 'Duke']
 
-
-
-for _ in range(20):
-    from itertools import cycle
-    from random import random
+for _ in range(200):
 
     testgame = Play_Coup(PLAYERS)
 
-    for i in cycle(range(PLAYERS)):
-        acting_player = testgame.players[i]
-        
+    for acting_player in cycle(testgame.players):
         if not acting_player.influence_remaining:
             continue
-        elif sum(1 for p in range(PLAYERS) if testgame.players[p].influence_remaining) == 1:
-            gamestate = ([i in testgame.players[p] for i in influences for p in range(PLAYERS)])
-            
-            win_result = ('Ambassador' in acting_player,
-                        'Assassin' in acting_player,
-                        'Captain' in acting_player,
-                        'Contessa' in acting_player,
-                        'Duke' in acting_player)
+        
+        try:
+            acting_player.select_opponent(testgame.players)
+        except IndexError:
+            gamestate = ([i in p for i in influences for p in testgame.players])
+            win_result = tuple(i in acting_player for i in influences)
 
             ds.addSample(gamestate, win_result)
             break
         
         while 1:
             try:
+                random_player = acting_player.select_opponent(testgame.players)
                 action = acting_player.random_naive_priority()
 
                 if action == 'steal':
-                    random_player = acting_player.select_opponent(testgame.players)
                     if (action in random_player.probable_blocks and random() > .24) or \
                         action in random_player.valid_blocks:
                         raise RethinkAction(action, acting_player, random_player)
@@ -56,7 +53,6 @@ for _ in range(20):
                     else:
                         acting_player.perform(action, random_player)
                 elif action == 'assassinate':
-                    random_player = acting_player.select_opponent(testgame.players)
                     if (action in random_player.probable_blocks and random() > .24) or \
                         action in random_player.valid_blocks:
                         raise RethinkAction(action, acting_player, random_player)
@@ -77,7 +73,6 @@ for _ in range(20):
                 elif action == 'exchange':
                     acting_player.perform(action, testgame.court_deck)
                 elif action == 'coup':
-                    random_player = acting_player.select_opponent(testgame.players)
                     position, random_target = random_player.random_remaining_influence
                     acting_player.perform(action, random_target)
                     random_player.remove_suspicion(str(random_target))
@@ -86,18 +81,16 @@ for _ in range(20):
             except (IllegalTarget, IllegalAction):
                 pass
             except BlockedAction:
-                break
+                continue
             except RethinkAction as e:
                 pass
-            except IndexError:
-                break
             else:
                 break
 
 
 
 t1 = time()
-trainer.trainEpochs(300)
+trainer.trainEpochs(200)
 print "Time PyBrain {}".format(time()-t1)
 
 #PRINT RESULTS
